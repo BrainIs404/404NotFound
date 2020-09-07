f1badf5c30e73fd858f922e8b6205452afe274b0
Victor Ngo <tminhngvt1998@gmail.com>
2020-09-07 12:37:20 +1000

    team progress merged

---
+++ js/client.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on("message", addMessages)
+
+$(() => {
+	$("#send").click(()=>{
+		sendMessage({
+			//name: $("#name").val(),
+			name: "TestUser",
+			message:$("#message").val()});
+	})
+	getMessages()
+})
+function addMessages(message){
+	$("#messages").append(`
+		<h4> ${message.name} </h4>
+		<p>  ${message.message} </p>`)
+}
+function getMessages(){
+	$.get('http://localhost:3000/messages', (data) => {
+		data.forEach(addMessages);
+	})
+}
+function sendMessage(message){
+	$.post('http://localhost:3000/messages', message)
+}
